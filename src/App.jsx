289cdc6c3953abc6dce4940ae921a6ae4cc01248
Hollajay <127396029+Hollajay@users.noreply.github.com>
2025-02-20 13:31:16 +0100

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div><h1 className='text-2xl text-blue-700 underline hover:text-red-400'>Hair salon</h1></div>
+    </>
+  )
+}
+
+export default App
